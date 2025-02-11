a86b981bf212669578b5f312fbca70d1229cbb48
Anexin Wilson <anexin777@gmail.com>
2025-02-11 13:54:59 -0800

    fixed the issues with itjobs.ca not showing name properly and and showing all jobs in a page

--- anexin.wilson_a01.py
+++ anexin.wilson_a01.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import certifi
 
 
-itjobs_url = " https://www.itjobs.ca/en/search-jobs/?location=British+Columbia&location-id=BC&location-type=2&search=1&sort_order=1&page=1"
+itjobs_url = "https://www.itjobs.ca/en/search-jobs/?location=British+Columbia&location-id=BC&location-type=2&search=1&sort_order=1&page=1"
 techtalent_url = "https://jobs.techtalent.ca/?k=information%20technology&l=British%20Columbia,%20Canada"
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
@@ -13,18 +13,18 @@ headers = {'User-Agent':USER_AGENT}
 itjobs_page = requests.get(itjobs_url,headers=headers , verify=certifi.where())
 
 itjobs_soup = BeautifulSoup(itjobs_page.content,"html.parser")
-results = itjobs_soup.find(id ='search-results')
+results = itjobs_soup.find('div',class_ ='result-ctn content')
 # print(results)
-itjobs_cards = results.find_all('div',class_='details_wrapper')
-itjob_position = results.find('a', class_='offer-name')
+itjobs_cards = results.find_all('div',class_='result-item external')
+# itjob_position = results.find('a', class_='offer-name')
 
 
 for itjob in itjobs_cards:
-    itjob_position = itjob.find('a', class_='offer-name')
-    itjob_company = itjob.find('a', class_='company')
-    itjob_location = itjob.find('a', class_='location')
-    itjob_description = itjob.find('p', class_ = 'offer-description')
-    print(f'Position: {itjob_position} is for company {itjob_company} in {itjob_location} and job description is {itjob_description}')
+    itjob_position = itjob.find('a', class_='offer-name').contents[0].strip()
+    itjob_company = itjob.find( 'a', class_='company').text.strip()
+    itjob_location = itjob.find('a', class_='location').text.strip()
+    itjob_description = itjob.find('p', class_ = 'offer-description').text.strip()
+    print(f'Position: {itjob_position} is for company {itjob_company} in {itjob_location} and job description is {itjob_description} \n \n')
 
 
 # print(itjobs_soup)
